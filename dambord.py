f79fe42fd1bc5f97026945dc579b63e804c1a8a4
oldmartijntje <oldmartijntje@gmai.com>
2022-02-16 12:11:35 +0100

    i did it, kinda

--- dambord.py
+++ dambord.py
@@ -1,26 +1,25 @@
-
+from cProfile import label
 import tkinter
+import random
 window = tkinter.Tk()
 blackOrWhite = 'black'
 tekst = ['a','b','c','d','e','f','g','h','i','j']
-class tile:
-    def __init__(self,bg,fg,text,column,row,ipadx,ipady):
-        self.label = tkinter.Label(window,bg = bg,text= text,fg = fg).grid(column=column, row=row, ipadx=ipadx, ipady=ipady)
-        
 
 for x in range(10):
     for y in range(10):
         if x % 2 == 0:
-            labelClass = tile(blackOrWhite,'red',f'{tekst[y]}{10-x}',y,x,20,10)
-            #labelClass.label.bind('<Button-1>',lambda event: labelClass.label.configure(fg='red'))
-            #both don't work
-            label = tkinter.Label(window)
-            label.configure(bg = blackOrWhite,text= f'{tekst[y]}{10-x}',fg = blackOrWhite)
-            label.bind('<Button-1>',lambda event: print(label['text']))
-            label.grid(column=y, row=x, ipadx=20, ipady=10)
+            label_id = str(random.random()).replace(".", "")
+            exec(f"label{label_id} = tkinter.Label(window)")
+            exec(f"label{label_id}.configure(bg = blackOrWhite,text= f'{tekst[y]}{10-x}',fg = blackOrWhite)")
+            exec(f"label{label_id}.bind('<Button-1>',lambda event: label{label_id}.configure(fg='red'))")
+            exec(f"label{label_id}.grid(column=y, row=x, ipadx=20, ipady=10)")
 
         else:
-            tkinter.Label(window,bg = blackOrWhite,text= f'{tekst[9-y]}{10-x}',fg = 'red').grid(column=9-y, row=x, ipadx=20, ipady=10)
+            label_id = str(random.random()).replace(".", "")
+            exec(f"label{label_id} = tkinter.Label(window)")
+            exec(f"label{label_id}.configure(bg = blackOrWhite,text= f'{tekst[9-y]}{10-x}',fg = blackOrWhite)")
+            exec(f"label{label_id}.bind('<Button-1>',lambda event: label{label_id}.configure(fg='red'))")
+            exec(f"label{label_id}.grid(column=9-y, row=x, ipadx=20, ipady=10)")
         if blackOrWhite == 'black': blackOrWhite = 'white'
         else: blackOrWhite = 'black'
 window.mainloop()
